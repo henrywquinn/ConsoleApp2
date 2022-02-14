696a12a7864df126b3b0d199678b05e6185831b0
henrywquinn <54520005+henrywquinn@users.noreply.github.com>
2022-02-15 09:01:19 +1300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    
+   
+    
+    class Program
+    {
+
+    
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first number?");
+            var num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your second number?");
+            var num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Secondary(65, 23));
+
+            Console.WriteLine(Secondary(num1, num2));
+
+
+
+
+        }
+
+       
+        public static int Secondary(int num1, int num2)
+        {
+            
+            int sum = num1 + num2;
+
+
+            return sum;
+
+        }
+
+
+
+    }
+
+
+
+}
